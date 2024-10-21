02a2389bde135429f6818be2522e29db60157fee
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-10-21 23:00:23 +0100

    watershed inprogress

--- pages/docs/_meta.ts
+++ pages/docs/_meta.ts
@@ -1,5 +1,6 @@
 export default {
   index: "Intro",
+  computerVision: "Computer Vision",
   examples: "Example",
   // examples: {
   //   display: "hidden",
---
+++ pages/docs/computerVision/__meta.ts
@@ -0,0 +1,3 @@
+export default {
+  WatershedAlgorithm: "Watershed Algorithm",
+}
